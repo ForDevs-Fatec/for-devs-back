c051017ad9758d2314fbc215b3c3edfcc4b15625
Daniel <daniel.2308@outlook.com>
2023-09-06 22:43:22 -0300

    feat: adição de authmiddleware para verificar se usuário está logado no sistema

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express"; 
 import { UserController } from "./controllers/UserController";
+import { authMiddleware } from "./middleware/authMiddleware";
 
 const router = Router();
 
-router.post('/user', new UserController().create);
-router.get('/users', new UserController().ListUser);
-router.put('/user', new UserController().updateUser);
-router.delete('/user/:id', new UserController().deleteUser);
+router.post('/user', authMiddleware, new UserController().create);
+router.get('/users', authMiddleware, new UserController().ListUser);
+router.put('/user', authMiddleware, new UserController().updateUser);
+router.delete('/user/:id', authMiddleware, new UserController().deleteUser);
 router.post('/user/login', new UserController().login);
 
 export default router
