240f4ce5c7efb96cd8b960cf79c334dcc320d590
vbuarque <viniciusbuarque17@gmail.com>
2023-09-27 12:33:35 -0300

    fix: corrigindo rotas

--- src/routes.ts
+++ src/routes.ts
@@ -6,7 +6,7 @@ import { DadosController } from "./controllers/DadosController";
 const router = Router();
 
 router.post('/user', new UserController().create);
-router.get('/users', authMiddleware, new UserController().ListUser);
+router.get('/users', new UserController().ListUser);
 router.put('/user/:id', authMiddleware, new UserController().updateUser);
 router.delete('/user/:id', authMiddleware, new UserController().deleteUser);
 router.post('/user/login', new UserController().login);
